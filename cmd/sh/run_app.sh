f0853db67cc8127cc3381b4b1e4d0a50e4af84d5
Oleg Mochalov <viverofffff@gmail.com>
2021-09-09 00:25:11 +0700

    Timeout added into all sh scripts

--- cmd/sh/run_app.sh
+++ cmd/sh/run_app.sh
@@ -3,6 +3,10 @@
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 /bin/sh $SCRIPTPATH/../sh/wait_for_postgresql.sh
+status=$?
+if [ $status -ne 0 ]; then
+  exit $status
+fi
 
 echo >&2 "Run migrations..."
 "$SCRIPTPATH"/../../bin/migrations
